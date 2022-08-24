e04d4381895554714323e87b5e42b8888f4fd221
Kateryna Skoryna <k.skoryna@gmail.com>
2022-08-24 09:28:02 +0200

    Added refs for project

---
+++ src/js/refs.js
@@ -0,0 +1,21 @@
+const form = document.querySelector('.header__form');
+const gallery = document.querySelector('.gallery__list');
+const galleryListEl = document.querySelector('.gallery__item');
+const modalCloseBtn = document.querySelector('.modal__close-btn');
+const modalWatchBtn = document.querySelector('.modal__watch-btn');
+const modalQueueBtn = document.querySelector('.modal__queueBtn');
+const footerTeamLink = document.querySelector('.footer__link');
+const myLibWatchBtn = document.querySelector('.header__watch-btn');
+const myLibQueueBtn = document.querySelector('.header__queue-btn');
+
+export {
+  form,
+  gallery,
+  galleryListEl,
+  modalCloseBtn,
+  modalWatchBtn,
+  modalQueueBtn,
+  footerTeamLink,
+  myLibWatchBtn,
+  myLibQueueBtn,
+};
