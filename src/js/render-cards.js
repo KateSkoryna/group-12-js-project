14f85b603710fab6960bed202cade02053783ac1
Mykhailenko <juliam4433@gmail.com>
2022-08-24 15:47:43 +0200

    Update render-cards.js
    изменила genres.name, добавила импорт/експорт

--- src/js/render-cards.js
+++ src/js/render-cards.js
@@ -1,18 +1,20 @@
+import { gallery } from './refs';
+
 // рендер трендовых фильмов
 
-function renderTrandFilms(data){
+export function renderTrandFilms(data){
     const markup = data.map(({poster_path, title, id, genres, year, vote_average}) => {
       return  `<li class="gallery__item" data-action="#">
     <div class="gallery__wrapper">
         <img
             class="gallery__img"
-            src="https://www.themoviedb.org/t/p/w500 ${poster_path}"
+            src="https://www.themoviedb.org/t/p/w500${poster_path}"
             alt="${title}"
             data-id="${id}">
     </div>
     <div class="gallery__thumb">
-        <h3 class="gallery__name">${title}</h3>
-        <p class="gallery__genres">${genres}</p>
+        <h3 class="gallery__name" data-id="${id}">${title}</h3>
+        <p class="gallery__genres">${genres.name}</p>
         <p class="gallery__year">${year}</p>
         <span class="gallery__vote">${vote_average}</span>
     </div>
