c313c42dbfc51a6d8cdd0feded43eb1b45893444
Mykhailenko <juliam4433@gmail.com>
2022-08-24 15:06:38 +0200

    Рендер трендовых фильмов
    рендер при загрузке страницы

---
+++ src/js/render-cards.js
@@ -0,0 +1,23 @@
+// рендер трендовых фильмов
+
+function renderTrandFilms(data){
+    const markup = data.map(({poster_path, title, id, genres, year, vote_average}) => {
+      return  `<li class="gallery__item" data-action="#">
+    <div class="gallery__wrapper">
+        <img
+            class="gallery__img"
+            src="https://www.themoviedb.org/t/p/w500 ${poster_path}"
+            alt="${title}"
+            data-id="${id}">
+    </div>
+    <div class="gallery__thumb">
+        <h3 class="gallery__name">${title}</h3>
+        <p class="gallery__genres">${genres}</p>
+        <p class="gallery__year">${year}</p>
+        <span class="gallery__vote">${vote_average}</span>
+    </div>
+</li>`;
+    }).join('');
+
+    gallery.insertAdjacentHTML('beforeend', markup);
+}
