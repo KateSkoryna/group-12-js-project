768f27efe62475f334ed0217ca5a01ea300b76e1
Mykhailenko <juliam4433@gmail.com>
2022-08-25 15:49:21 +0200

    update fetch-films.js
    добавила пустую функцию и функцию поиска по id

--- src/js/fetch-films.js
+++ src/js/fetch-films.js
@@ -2,7 +2,21 @@ import axios from 'axios';
 
 const KEY = 'fd7341fdf0f2e94a335192ece09ea376';
 
-export async function fetchTrendFilms(page) {
+// функция без аргумента 
+async function fetchAllFilms() {
+    const url = 'https://api.themoviedb.org/3/trending/movie/week';
+
+    try {
+      const { data } = await axios.get(`${url}?api_key=${KEY}`);
+        return data;
+        
+    } catch (error) {
+      console.error('Something wrong! Can not get full trends' + error);
+    }
+}
+
+// функция для пагинации
+async function fetchTrendFilms(page) {
     const url = 'https://api.themoviedb.org/3/trending/movie/week';
 
     try {
@@ -14,7 +28,8 @@ export async function fetchTrendFilms(page) {
     }
 }
   
-export async function fetchSearchingFilms(text, page) {
+// функция для поиска по названию
+async function fetchSearchFilms(text, page) {
     const url = 'https://api.themoviedb.org/3/search/movie';
 
     try {
@@ -25,3 +40,18 @@ export async function fetchSearchingFilms(text, page) {
       console.error('Something wrong! Can not search films' + error);
     }
 }
+
+// функция для поиска по id
+async function fetchSearchFilmById(id) {
+  const url = 'https://api.themoviedb.org/3/movie';
+
+  try {
+    const { data } = await axios.get(`${url}${id}?api_key=${KEY}`);
+      return data;
+    
+  } catch (error) {
+    console.error('Something wrong! Can not search films by ID' + error);
+    }
+}
+
+export { fetchAllFilms, fetchTrendFilms, fetchSearchFilms, fetchSearchFilmById }; 
